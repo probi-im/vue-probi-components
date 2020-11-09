5198dc736e86dbcdc4df0fe54ab3c3c92b95d9b1
Bastien <probicieux44@gmail.com>
2020-11-09 23:53:07 +0100

    correctly added input component

--- src/entry.js
+++ src/entry.js
@@ -1,11 +1,13 @@
 import ProbiButton from './components/Button.vue';
 import ProbiCard from './components/Card.vue';
 import ProbiCardHover from './components/CardHover.vue';
+import ProbiInput from './components/Input.vue';
 
 const components = {
   ProbiButton,
   ProbiCard,
   ProbiCardHover,
+  ProbiInput
 };
 
 export default components;
